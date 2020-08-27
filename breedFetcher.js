fb4d3ad1e1eb579b39dad490b7892b91dbe38cf9
Audrey Cooper <audrey_cooper@me.com>
2020-08-27 15:15:15 -0700

    re factoring  unfinished, test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,37 @@
 const request = require('request');
+const breedName = process.argv[2];
 
-const userInput = process.argv[2];
+const fetchBreedDescription = function (breedName, callback) {
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
-// console.log(url)
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  //console.log(url)
+  let desc = '';
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
+  request(url, (error, response, body) => {
+
+    // if (error) {
+    // console.log('error:', error); // Print the error if one occurred
+    // } else {
+    //console.log('body: ', body)
     const data = JSON.parse(body);
+    //console.log(data)
     if (data[0] === undefined) {
-      console.log('breed not found');
+      desc = 'breed not found';
+      // console.log(desc)
+      return desc
     } else {
-      console.log(data[0]['description']);
+      desc = (data[0]['description']);
+      // console.log(desc)
+      return desc
     }
+  });
+
+};
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
   }
 });
-
